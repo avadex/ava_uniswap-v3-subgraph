a7b7b44e42331d5a3cca2af470c33281955db2d1
avadex <wavax@protonmail.com>
2021-07-04 04:44:59 -0400

    up

--- src/mappings/core.ts
+++ src/mappings/core.ts
@@ -1,16 +1,28 @@
 /* eslint-disable prefer-const */
-import { Bundle, Pool, Token, Factory, Mint, Burn, Swap, Tick } from '../types/schema'
-import { BigDecimal, BigInt, log, store } from '@graphprotocol/graph-ts'
-import { Mint as MintEvent, Burn as BurnEvent, Swap as SwapEvent, Initialize } from '../types/templates/Pool/Pool'
-import { convertTokenToDecimal, loadTransaction } from '../utils'
+import { Bundle, Burn, Factory, Mint, Pool, Swap, Tick, Token } from '../types/schema'
+import { Pool as PoolABI } from '../types/Factory/Pool'
+import { BigDecimal, BigInt, ethereum, store } from '@graphprotocol/graph-ts'
+import {
+  Burn as BurnEvent,
+  Flash as FlashEvent,
+  Initialize,
+  Mint as MintEvent,
+  Swap as SwapEvent
+} from '../types/templates/Pool/Pool'
+import { convertTokenToDecimal, loadTransaction, safeDiv } from '../utils'
 import { FACTORY_ADDRESS, ONE_BI, ZERO_BD, ZERO_BI } from '../utils/constants'
-import { findEthPerToken, getEthPriceInUSD, sqrtPriceX96ToTokenPrices } from '../utils/pricing'
-import { updateUniswapDayData, updatePoolDayData, updateTokenDayData } from '../utils/intervalUpdates'
-import { createTick } from '../utils/tick'
+import { findEthPerToken, getEthPriceInUSD, getTrackedAmountUSD, sqrtPriceX96ToTokenPrices } from '../utils/pricing'
+import {
+  updatePoolDayData,
+  updatePoolHourData,
+  updateTickDayData,
+  updateTokenDayData,
+  updateTokenHourData,
+  updateUniswapDayData
+} from '../utils/intervalUpdates'
+import { createTick, feeTierToTickSpacing } from '../utils/tick'
 
 export function handleInitialize(event: Initialize): void {
-  log.debug('mynewbug initializing', [])
-
   let pool = Pool.load(event.address.toHexString())
   pool.sqrtPrice = event.params.sqrtPriceX96
   pool.tick = BigInt.fromI32(event.params.tick)
@@ -24,6 +36,7 @@ export function handleInitialize(event: Initialize): void {
   bundle.save()
 
   updatePoolDayData(event)
+  updatePoolHourData(event)
 
   // update token prices
   token0.derivedETH = findEthPerToken(token0 as Token)
@@ -134,16 +147,21 @@ export function handleMint(event: MintEvent): void {
 
   updateUniswapDayData(event)
   updatePoolDayData(event)
+  updatePoolHourData(event)
   updateTokenDayData(token0 as Token, event)
   updateTokenDayData(token1 as Token, event)
+  updateTokenHourData(token0 as Token, event)
+  updateTokenHourData(token1 as Token, event)
 
   token0.save()
   token1.save()
   pool.save()
   factory.save()
   mint.save()
-  lowerTick.save()
-  upperTick.save()
+
+  // Update inner tick vars and save the ticks
+  updateTickFeeVarsAndSave(lowerTick!, event)
+  updateTickFeeVarsAndSave(upperTick!, event)
 }
 
 export function handleBurn(event: BurnEvent): void {
@@ -231,22 +249,13 @@ export function handleBurn(event: BurnEvent): void {
 
   updateUniswapDayData(event)
   updatePoolDayData(event)
+  updatePoolHourData(event)
   updateTokenDayData(token0 as Token, event)
   updateTokenDayData(token1 as Token, event)
-
-  // If liquidity gross is zero then there are no positions starting at or ending at the tick.
-  // It is now safe to remove the tick from the data store.
-  if (lowerTick.liquidityGross.equals(ZERO_BI)) {
-    store.remove('Tick', lowerTickId)
-  } else {
-    lowerTick.save()
-  }
-
-  if (upperTick.liquidityGross.equals(ZERO_BI)) {
-    store.remove('Tick', upperTickId)
-  } else {
-    upperTick.save()
-  }
+  updateTokenHourData(token0 as Token, event)
+  updateTokenHourData(token1 as Token, event)
+  updateTickFeeVarsAndSave(lowerTick!, event)
+  updateTickFeeVarsAndSave(upperTick!, event)
 
   token0.save()
   token1.save()
@@ -259,8 +268,16 @@ export function handleSwap(event: SwapEvent): void {
   let bundle = Bundle.load('1')
   let factory = Factory.load(FACTORY_ADDRESS)
   let pool = Pool.load(event.address.toHexString())
+
+  // hot fix for bad pricing
+  if (pool.id == '0x1C20E891Bab6b1727d14Da358FAe2984Ed9B59EB') {
+    return
+  }
+
   let token0 = Token.load(pool.token0)
   let token1 = Token.load(pool.token1)
+
+  let oldTick = pool.tick!
 
   // amounts - 0/1 are token deltas: can be positive or negative
   let amount0 = convertTokenToDecimal(event.params.amount0, token0.decimals)
@@ -281,24 +298,21 @@ export function handleSwap(event: SwapEvent): void {
   let amount0USD = amount0ETH.times(bundle.ethPriceUSD)
   let amount1USD = amount1ETH.times(bundle.ethPriceUSD)
 
-  /**
-   * @todo
-   * need to account for if either amount is more reliable
-   */
-  let amountTotalETH = amount0ETH.plus(amount1ETH).div(BigDecimal.fromString('2'))
-  let amountTotalUSD = amount0USD.plus(amount1USD).div(BigDecimal.fromString('2'))
+  // get amount that should be tracked only - div 2 because cant count both input and output as volume
+  let amountTotalUSDTracked = getTrackedAmountUSD(amount0Abs, token0 as Token, amount1Abs, token1 as Token).div(
+    BigDecimal.fromString('2')
+  )
+  let amountTotalETHTracked = safeDiv(amountTotalUSDTracked, bundle.ethPriceUSD)
+  let amountTotalUSDUntracked = amount0USD.plus(amount1USD).div(BigDecimal.fromString('2'))
 
-  let feesETH = amountTotalETH
-    .times(pool.feeTier.toBigDecimal())
-    .div(BigDecimal.fromString('1000000'))
-  let feesUSD = amountTotalUSD
-    .times(pool.feeTier.toBigDecimal())
-    .div(BigDecimal.fromString('1000000'))
+  let feesETH = amountTotalETHTracked.times(pool.feeTier.toBigDecimal()).div(BigDecimal.fromString('1000000'))
+  let feesUSD = amountTotalUSDTracked.times(pool.feeTier.toBigDecimal()).div(BigDecimal.fromString('1000000'))
 
   // global updates
   factory.txCount = factory.txCount.plus(ONE_BI)
-  factory.totalVolumeETH = factory.totalVolumeETH.plus(amount0ETH).plus(amount1ETH)
-  factory.totalVolumeUSD = factory.totalVolumeUSD.plus(amountTotalUSD)
+  factory.totalVolumeETH = factory.totalVolumeETH.plus(amountTotalETHTracked)
+  factory.totalVolumeUSD = factory.totalVolumeUSD.plus(amountTotalUSDTracked)
+  factory.untrackedVolumeUSD = factory.untrackedVolumeUSD.plus(amountTotalUSDUntracked)
   factory.totalFeesETH = factory.totalFeesETH.plus(feesETH)
   factory.totalFeesUSD = factory.totalFeesUSD.plus(feesUSD)
 
@@ -307,9 +321,10 @@ export function handleSwap(event: SwapEvent): void {
   factory.totalValueLockedETH = factory.totalValueLockedETH.minus(currentPoolTvlETH)
 
   // pool volume
-  pool.volumeToken0 = pool.volumeToken0.plus(amount0)
-  pool.volumeToken1 = pool.volumeToken1.plus(amount1)
-  pool.volumeUSD = pool.volumeUSD.plus(amountTotalUSD)
+  pool.volumeToken0 = pool.volumeToken0.plus(amount0Abs)
+  pool.volumeToken1 = pool.volumeToken1.plus(amount1Abs)
+  pool.volumeUSD = pool.volumeUSD.plus(amountTotalUSDTracked)
+  pool.untrackedVolumeUSD = pool.untrackedVolumeUSD.plus(amountTotalUSDUntracked)
   pool.feesUSD = pool.feesUSD.plus(feesUSD)
   pool.txCount = pool.txCount.plus(ONE_BI)
 
@@ -323,14 +338,18 @@ export function handleSwap(event: SwapEvent): void {
   // update token0 data
   token0.volume = token0.volume.plus(amount0Abs)
   token0.totalValueLocked = token0.totalValueLocked.plus(amount0)
-  token0.volumeUSD = token0.volumeUSD.plus(amountTotalUSD)
+  token0.volumeUSD = token0.volumeUSD.plus(amountTotalUSDTracked)
+  token0.untrackedVolumeUSD = token0.untrackedVolumeUSD.plus(amountTotalUSDUntracked)
   token0.feesUSD = token0.feesUSD.plus(feesUSD)
+  token0.txCount = token0.txCount.plus(ONE_BI)
 
   // update token1 data
   token1.volume = token1.volume.plus(amount1Abs)
   token1.totalValueLocked = token1.totalValueLocked.plus(amount1)
-  token1.volumeUSD = token1.volumeUSD.plus(amountTotalUSD)
+  token1.volumeUSD = token1.volumeUSD.plus(amountTotalUSDTracked)
+  token1.untrackedVolumeUSD = token1.untrackedVolumeUSD.plus(amountTotalUSDUntracked)
   token1.feesUSD = token1.feesUSD.plus(feesUSD)
+  token1.txCount = token1.txCount.plus(ONE_BI)
 
   // updated pool ratess
   let prices = sqrtPriceX96ToTokenPrices(pool.sqrtPrice, token0 as Token, token1 as Token)
@@ -338,28 +357,9 @@ export function handleSwap(event: SwapEvent): void {
   pool.token1Price = prices[1]
   pool.save()
 
-  // log.debug(
-  //   'mybug -  hash: {}, amount0: {}, amount1: {}, token0DE: {}, token1DE: {}, sqrtPrice: {}, price0: {}, price1: {}, locked0: {}, locked1: {}, new liquidity: {}',
-  //   [
-  //     event.transaction.hash.toString(),
-  //     amount0.toString(),
-  //     amount1.toString(),
-  //     token0.derivedETH.toString(),
-  //     token1.derivedETH.toString(),
-  //     event.params.sqrtPriceX96.toString(),
-  //     pool.token0Price.toString(),
-  //     pool.token1Price.toString(),
-  //     pool.totalValueLockedToken0.toString(),
-  //     pool.totalValueLockedToken1.toString(),
-  //     event.params.liquidity.toString()
-  //   ]
-  // )
-
-  // update eth price
+  // update USD pricing
   bundle.ethPriceUSD = getEthPriceInUSD()
   bundle.save()
-
-  // update token derived rates
   token0.derivedETH = findEthPerToken(token0 as Token)
   token1.derivedETH = findEthPerToken(token1 as Token)
 
@@ -390,36 +390,61 @@ export function handleSwap(event: SwapEvent): void {
   swap.recipient = event.params.recipient
   swap.amount0 = amount0
   swap.amount1 = amount1
-  swap.amountUSD = amountTotalUSD
+  swap.amountUSD = amountTotalUSDTracked
   swap.tick = BigInt.fromI32(event.params.tick as i32)
   swap.sqrtPriceX96 = event.params.sqrtPriceX96
   swap.logIndex = event.logIndex
 
+  // update fee growth
+  let poolContract = PoolABI.bind(event.address)
+  let feeGrowthGlobal0X128 = poolContract.feeGrowthGlobal0X128()
+  let feeGrowthGlobal1X128 = poolContract.feeGrowthGlobal1X128()
+  pool.feeGrowthGlobal0X128 = feeGrowthGlobal0X128 as BigInt
+  pool.feeGrowthGlobal1X128 = feeGrowthGlobal1X128 as BigInt
+
   // interval data
   let uniswapDayData = updateUniswapDayData(event)
   let poolDayData = updatePoolDayData(event)
+  let poolHourData = updatePoolHourData(event)
   let token0DayData = updateTokenDayData(token0 as Token, event)
   let token1DayData = updateTokenDayData(token1 as Token, event)
+  let token0HourData = updateTokenHourData(token0 as Token, event)
+  let token1HourData = updateTokenHourData(token1 as Token, event)
 
   // update volume metrics
-  uniswapDayData.volumeETH = uniswapDayData.volumeETH.plus(amountTotalETH)
-  uniswapDayData.volumeUSD = uniswapDayData.volumeUSD.plus(amountTotalUSD)
+  uniswapDayData.volumeETH = uniswapDayData.volumeETH.plus(amountTotalETHTracked)
+  uniswapDayData.volumeUSD = uniswapDayData.volumeUSD.plus(amountTotalUSDTracked)
   uniswapDayData.feesUSD = uniswapDayData.feesUSD.plus(feesUSD)
 
-  poolDayData.volumeUSD = poolDayData.volumeUSD.plus(amountTotalUSD)
+  poolDayData.volumeUSD = poolDayData.volumeUSD.plus(amountTotalUSDTracked)
   poolDayData.volumeToken0 = poolDayData.volumeToken0.plus(amount0Abs)
   poolDayData.volumeToken1 = poolDayData.volumeToken1.plus(amount1Abs)
   poolDayData.feesUSD = poolDayData.feesUSD.plus(feesUSD)
 
+  poolHourData.volumeUSD = poolHourData.volumeUSD.plus(amountTotalUSDTracked)
+  poolHourData.volumeToken0 = poolHourData.volumeToken0.plus(amount0Abs)
+  poolHourData.volumeToken1 = poolHourData.volumeToken1.plus(amount1Abs)
+  poolHourData.feesUSD = poolHourData.feesUSD.plus(feesUSD)
+
   token0DayData.volume = token0DayData.volume.plus(amount0Abs)
-  token0DayData.volumeUSD = token0DayData.volumeUSD.plus(amountTotalUSD)
-  token0DayData.untrackedVolumeUSD = token0DayData.untrackedVolumeUSD.plus(amountTotalUSD)
+  token0DayData.volumeUSD = token0DayData.volumeUSD.plus(amountTotalUSDTracked)
+  token0DayData.untrackedVolumeUSD = token0DayData.untrackedVolumeUSD.plus(amountTotalUSDTracked)
   token0DayData.feesUSD = token0DayData.feesUSD.plus(feesUSD)
 
+  token0HourData.volume = token0HourData.volume.plus(amount0Abs)
+  token0HourData.volumeUSD = token0HourData.volumeUSD.plus(amountTotalUSDTracked)
+  token0HourData.untrackedVolumeUSD = token0HourData.untrackedVolumeUSD.plus(amountTotalUSDTracked)
+  token0HourData.feesUSD = token0HourData.feesUSD.plus(feesUSD)
+
   token1DayData.volume = token1DayData.volume.plus(amount1Abs)
-  token1DayData.volumeUSD = token1DayData.volumeUSD.plus(amountTotalUSD)
-  token1DayData.untrackedVolumeUSD = token1DayData.untrackedVolumeUSD.plus(amountTotalUSD)
+  token1DayData.volumeUSD = token1DayData.volumeUSD.plus(amountTotalUSDTracked)
+  token1DayData.untrackedVolumeUSD = token1DayData.untrackedVolumeUSD.plus(amountTotalUSDTracked)
   token1DayData.feesUSD = token1DayData.feesUSD.plus(feesUSD)
+
+  token1HourData.volume = token1HourData.volume.plus(amount1Abs)
+  token1HourData.volumeUSD = token1HourData.volumeUSD.plus(amountTotalUSDTracked)
+  token1HourData.untrackedVolumeUSD = token1HourData.untrackedVolumeUSD.plus(amountTotalUSDTracked)
+  token1HourData.feesUSD = token1HourData.feesUSD.plus(feesUSD)
 
   swap.save()
   token0DayData.save()
@@ -430,4 +455,72 @@ export function handleSwap(event: SwapEvent): void {
   pool.save()
   token0.save()
   token1.save()
+
+  // Update inner vars of current or crossed ticks
+  let newTick = pool.tick!
+  let tickSpacing = feeTierToTickSpacing(pool.feeTier)
+  let modulo = newTick.mod(tickSpacing)
+  if (modulo.equals(ZERO_BI)) {
+    // Current tick is initialized and needs to be updated
+    loadTickUpdateFeeVarsAndSave(newTick.toI32(), event)
+  }
+
+  let numIters = oldTick
+    .minus(newTick)
+    .abs()
+    .div(tickSpacing)
+
+  if (numIters.gt(BigInt.fromI32(100))) {
+    // In case more than 100 ticks need to be updated ignore the update in
+    // order to avoid timeouts. From testing this behavior occurs only upon
+    // pool initialization. This should not be a big issue as the ticks get
+    // updated later. For early users this error also disappears when calling
+    // collect
+  } else if (newTick.gt(oldTick)) {
+    let firstInitialized = oldTick.plus(tickSpacing.minus(modulo))
+    for (let i = firstInitialized; i.le(newTick); i = i.plus(tickSpacing)) {
+      loadTickUpdateFeeVarsAndSave(i.toI32(), event)
+    }
+  } else if (newTick.lt(oldTick)) {
+    let firstInitialized = oldTick.minus(modulo)
+    for (let i = firstInitialized; i.ge(newTick); i = i.minus(tickSpacing)) {
+      loadTickUpdateFeeVarsAndSave(i.toI32(), event)
+    }
+  }
+}
+
+export function handleFlash(event: FlashEvent): void {
+  // update fee growth
+  let pool = Pool.load(event.address.toHexString())
+  let poolContract = PoolABI.bind(event.address)
+  let feeGrowthGlobal0X128 = poolContract.feeGrowthGlobal0X128()
+  let feeGrowthGlobal1X128 = poolContract.feeGrowthGlobal1X128()
+  pool.feeGrowthGlobal0X128 = feeGrowthGlobal0X128 as BigInt
+  pool.feeGrowthGlobal1X128 = feeGrowthGlobal1X128 as BigInt
+  pool.save()
+}
+
+function updateTickFeeVarsAndSave(tick: Tick, event: ethereum.Event): void {
+  let poolAddress = event.address
+  // not all ticks are initialized so obtaining null is expected behavior
+  let poolContract = PoolABI.bind(poolAddress)
+  let tickResult = poolContract.ticks(tick.tickIdx.toI32())
+  tick.feeGrowthOutside0X128 = tickResult.value2
+  tick.feeGrowthOutside1X128 = tickResult.value3
+  tick.save()
+
+  updateTickDayData(tick!, event)
+}
+
+function loadTickUpdateFeeVarsAndSave(tickId: i32, event: ethereum.Event): void {
+  let poolAddress = event.address
+  let tick = Tick.load(
+    poolAddress
+      .toHexString()
+      .concat('#')
+      .concat(tickId.toString())
+  )
+  if (tick !== null) {
+    updateTickFeeVarsAndSave(tick!, event)
+  }
 }
--- src/utils/pricing.ts
+++ src/utils/pricing.ts
@@ -2,22 +2,24 @@
 import { ONE_BD, ZERO_BD, ZERO_BI } from './constants'
 import { Bundle, Pool, Token } from './../types/schema'
 import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
-import { exponentToBigDecimal } from '../utils/index'
+import { exponentToBigDecimal, safeDiv } from '../utils/index'
 
 const WETH_ADDRESS = '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7'
-// const DAI_WETH_03_POOL = '0xc2e9f25be6257c210d7adf0d4cd6e3e881ba25f8'
-const USDC_WETH_03_POOL = '0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8'
+const USDT_WETH_05_POOL = '0x852767337A1a2A5d0e5b9EeEAbBDc12F882A69bC' //aeb usdt/wavax pool
 
 // token where amounts should contribute to tracked volume and liquidity
+// usually tokens that many tokens are paired with s
 export let WHITELIST_TOKENS: string[] = [
   WETH_ADDRESS, // WETH
-  '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7', // WETH
-  '0xde3A24028580884448a5397872046a019649b084', // usdt aeb
-  '0xbA7dEebBFC5fA1100Fb055a87773e1E99Cd3507a' // DAI   aeb
-
+  '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7', // WETH  /WAVAX
+  '0xbA7dEebBFC5fA1100Fb055a87773e1E99Cd3507a', // DAI   aeb
+  '0xde3A24028580884448a5397872046a019649b084', // USDT aeb
+  '0x0000000000085d4780b73119b644ae5ecd22b376', // TUSD aeb
+  '0x408D4cD0ADb7ceBd1F1A1C33A0Ba2098E1295bAB', // WBTC aeb
+  '0x60781C2586D68229fde47564546784ab3fACA982' // PNG aeb
 ]
 
-let MINIMUM_ETH_LOCKED = BigDecimal.fromString('80')
+let MINIMUM_ETH_LOCKED = BigDecimal.fromString('0')
 
 let Q192 = 2 ** 192
 export function sqrtPriceX96ToTokenPrices(sqrtPriceX96: BigInt, token0: Token, token1: Token): BigDecimal[] {
@@ -28,16 +30,15 @@ export function sqrtPriceX96ToTokenPrices(sqrtPriceX96: BigInt, token0: Token, t
     .times(exponentToBigDecimal(token0.decimals))
     .div(exponentToBigDecimal(token1.decimals))
 
-  let price0 = BigDecimal.fromString('1').div(price1)
-
+  let price0 = safeDiv(BigDecimal.fromString('1'), price1)
   return [price0, price1]
 }
 
 export function getEthPriceInUSD(): BigDecimal {
   // fetch eth prices for each stablecoin
-  let usdcPool = Pool.load(USDC_WETH_03_POOL) // dai is token0
-  if (usdcPool !== null) {
-    return usdcPool.token0Price
+  let usdtPool = Pool.load(USDT_WETH_05_POOL) // dai is token0
+  if (usdtPool !== null) {
+    return usdtPool.token0Price
   } else {
     return ZERO_BD
   }
@@ -65,7 +66,6 @@ export function findEthPerToken(token: Token): BigDecimal {
         let token1 = Token.load(pool.token1)
         // get the derived ETH in pool
         let ethLocked = pool.totalValueLockedToken1.times(token1.derivedETH)
-
         if (ethLocked.gt(largestLiquidityETH) && ethLocked.gt(MINIMUM_ETH_LOCKED)) {
           largestLiquidityETH = ethLocked
           // token1 per our token * Eth per token1
@@ -93,31 +93,31 @@ export function findEthPerToken(token: Token): BigDecimal {
  * If both are, return sum of two amounts
  * If neither is, return 0
  */
-export function getTrackedLiquidityUSD(
+export function getTrackedAmountUSD(
   tokenAmount0: BigDecimal,
   token0: Token,
   tokenAmount1: BigDecimal,
   token1: Token
 ): BigDecimal {
   let bundle = Bundle.load('1')
-  let price0 = token0.derivedETH.times(bundle.ethPriceUSD)
-  let price1 = token1.derivedETH.times(bundle.ethPriceUSD)
+  let price0USD = token0.derivedETH.times(bundle.ethPriceUSD)
+  let price1USD = token1.derivedETH.times(bundle.ethPriceUSD)
 
-  // both are whitelist tokens, take average of both amounts
+  // both are whitelist tokens, return sum of both amounts
   if (WHITELIST_TOKENS.includes(token0.id) && WHITELIST_TOKENS.includes(token1.id)) {
-    return tokenAmount0.times(price0).plus(tokenAmount1.times(price1))
+    return tokenAmount0.times(price0USD).plus(tokenAmount1.times(price1USD))
   }
 
   // take double value of the whitelisted token amount
   if (WHITELIST_TOKENS.includes(token0.id) && !WHITELIST_TOKENS.includes(token1.id)) {
-    return tokenAmount0.times(price0).times(BigDecimal.fromString('2'))
+    return tokenAmount0.times(price0USD).times(BigDecimal.fromString('2'))
   }
 
   // take double value of the whitelisted token amount
   if (!WHITELIST_TOKENS.includes(token0.id) && WHITELIST_TOKENS.includes(token1.id)) {
-    return tokenAmount1.times(price1).times(BigDecimal.fromString('2'))
+    return tokenAmount1.times(price1USD).times(BigDecimal.fromString('2'))
   }
 
-  // neither token is on white list, tracked volume is 0
+  // neither token is on white list, tracked amount is 0
   return ZERO_BD
 }
