70f5480d2659864a4f123e70f73f75c540440f4d
avadex <85082306+avadex@users.noreply.github.com>
2022-01-24 03:48:06 +0100

    1 bp

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Uniswap V3 Subgraph
 
+### Subgraph Endpoint 
+
+Synced at: https://thegraph.com/hosted-service/subgraph/avadex/v35?selected=playground
+
+Pending Changes at same URL
---
+++ src/utils/constants.ts
@@ -0,0 +1,14 @@
+/* eslint-disable prefer-const */
+import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
+import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
+
+export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+export const FACTORY_ADDRESS = '0xE06B9a3C0A4314E00B33d9090a190ddC00a4DD01'
+
+export let ZERO_BI = BigInt.fromI32(0)
+export let ONE_BI = BigInt.fromI32(1)
+export let ZERO_BD = BigDecimal.fromString('0')
+export let ONE_BD = BigDecimal.fromString('1')
+export let BI_18 = BigInt.fromI32(18)
+
+export let factoryContract = FactoryContract.bind(Address.fromString(FACTORY_ADDRESS))
